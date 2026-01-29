c52ed5fe9ede41dd4d7cee6a2f2c6d8306a5cf8f
Babilabong <157375371+Babilabong@users.noreply.github.com>
2026-01-29 12:02:42 +0200

    update cors in backend

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDb } from "./db.js";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors({
+  origin: "*", // מאפשר גישה מכל מקום (לצורך פיתוח)
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}));
+app.options('*', cors());
 app.use(express.json());
 
 await initDb();
